91a3dc36d5ad0e031c9622d54bef709b44cb5f75
JEthebest <iskokairatov@gmail.com>
2023-01-30 23:10:15 +0600

    fix bug

--- objects_app/views.py
+++ objects_app/views.py
@@ -3,28 +3,27 @@ from django.urls.conf import include
 
 from .models import Item
 from .serializers import ItemSerializer
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from django.http import Http404
 
-class ItemListView(APIView):
+class ItemsListView(APIView):
     def get(self,request,format = None):
         snippets = Item.objects.all()
         serializer =ItemSerializer(snippets ,many = True)
         return Response(serializer.data)
 
-    
-    def post(self, request):
-        fool = request.data.get('fool')
-        seralizer = ItemSerializer(data=fool)
-        if seralizer.is_valid(raise_exception=True):
-            fool_saved = seralizer.save()
+    def post(self,request,format= None):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({"success": "fool '{}' created successfully".format(fool_saved)})
-
-
-class ItemDetailView(APIView):
+class ItemsDetailView(APIView):
     def get_object(self,pk):
         try:
             return Item.objects.get(pk=pk)
